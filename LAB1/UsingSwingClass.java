d5ed31ab76c9313e2dcb5a00bf0e356a9bc8d0eb
Vlad1slav <svlad1999@ukr.net>
2019-09-05 14:44:09 +0300

    Add logic

--- LAB1/UsingSwingClass.java
+++ LAB1/UsingSwingClass.java
@@ -5,13 +5,33 @@ public class UsingSwingClass {
 	public static void main(String[] args) {
 		UsingSwingClass lab1 = new UsingSwingClass();
 		short operators[] = lab1.askValues();
-
+		System.out.println(operators[0]);
 		double sum = lab1.calculate(operators[0], operators[1], operators[2], operators[3]); 
-
+		System.out.println(sum);
 		JOptionPane.showMessageDialog(null, "the sum is : " + sum , "Results", JOptionPane.PLAIN_MESSAGE );
+		System.exit(0);
 	}
 	public double calculate(short numberI, short numberJ, short numberN, short numberM) {
-		return 2.15;
+		try {
+			double sum = 0;
+		// short i = numberI;
+		short j = numberJ;
+		System.out.println(numberI);
+		while (numberI < numberN) {
+			j = numberJ;
+			while (j < numberM) {
+				sum += (numberI / j) / (numberI - 3);
+				System.out.print((numberI / j) / (numberI - 3));
+				j++;
+			}
+			numberI++;
+		}
+		return sum;
+		} catch (Exception e) {
+			JOptionPane.showMessageDialog(null, e, "title", JOptionPane.PLAIN_MESSAGE);
+			System.exit(1);
+		}
+		return 0;
 	}
 
 	public short[] askValues() {
@@ -37,6 +57,7 @@ public class UsingSwingClass {
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 			JOptionPane.showMessageDialog(null, e, "title", JOptionPane.PLAIN_MESSAGE);
+			System.exit(1);
 		}
 		return null;
 	}
